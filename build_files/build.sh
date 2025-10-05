380b7c15121fd5af0f1f98fdaaf5dc8c9786935e
muralidharan <muralidramachandran@gmail.com>
2025-10-05 20:13:47 +0530

    copy redundant in buildsh.

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,7 @@ dnf5 install -y rsync
 # Copy fsroot/ to /
 rsync -rvK /ctx/fsroot/ /
 
-cp /ctx/scripts /tmp/ctx/scripts
+cp -r /ctx/scripts /tmp/ctx/scripts
 chmod -x /tmp/ctx/scripts/*
 
 /tmp/ctx/scripts/01-copr.sh
