195b7e12bea9a3d4b9e78c0efb0c8cc1af1f1d8a
muralidharan <muralidramachandran@gmail.com>
2025-10-05 20:34:27 +0530

    updated zram config

--- build_files/scripts/03-mods.sh
+++ build_files/scripts/03-mods.sh
@@ -4,8 +4,8 @@ set -ouex pipefail
 
 # modifications to /etc/
 # ZRAM conf
-cp /usr/lib/systemd/zram-generator.conf /usr/lib/systemd/zram-generator.conf.bkp
-echo -e "\n# Default algorithm changed from lzo-rle to zstd \ncompression-algorithm = zstd" | tee -a /usr/lib/systemd/zram-generator.conf
+# cp /usr/lib/systemd/zram-generator.conf /usr/lib/systemd/zram-generator.conf.bkp
+echo -e "# New ZRAM config\n[zram0]\nzram-size = min(ram, 16384)\ncompression-algorithm = zstd" | tee -a /usr/lib/systemd/zram-generator.conf
 echo -e "# zram conf copied from PopOS\nvm.swappiness = 180\nvm.watermark_boost_factor = 0\nvm.watermark_scale_factor = 125\nvm.page-cluster = 0" | tee -a /etc/sysctl.d/99-vm-zram-parameters.conf
 
 # /etc/udev/rules.d/r_battery.rules to limit charging to 85%
